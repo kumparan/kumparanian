e4c9b1d3226f32c0cd9b8fd9c75d8af223b0ce3b
ghefira <ghefira.fatimah@kumparan.com>
2024-05-29 10:06:52 +0700

    feature: remove "python -m", add "poetry build" in ci.yml, add [tool.poetry.scripts] in pyproject.toml

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -6,42 +6,42 @@ class TestCLI(unittest.TestCase):
     def test_kumparanian(self):
         with open("tests/kumparanian.output") as file:
             expected_output = file.read()
-        command = "python -m kumparanian"
+        command = "kumparanian"
         output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
         self.assertEqual(expected_output, output.decode("utf-8"))
 
     def test_kumparanian_help(self):
         with open("tests/kumparanian_help.output") as file:
             expected_output = file.read()
-        command = "python -m kumparanian --help"
+        command = "kumparanian --help"
         output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
         self.assertEqual(expected_output, output.decode("utf-8"))
 
     def test_kumparanian_ds(self):
         with open("tests/kumparanian_ds.output") as file:
             expected_output = file.read()
-        command = "python -m kumparanian ds"
+        command = "kumparanian ds"
         output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
         self.assertEqual(expected_output, output.decode("utf-8"))
 
     def test_kumparanian_ds_help(self):
         with open("tests/kumparanian_ds_help.output") as file:
             expected_output = file.read()
-        command = "python -m kumparanian ds --help"
+        command = "kumparanian ds --help"
         output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
         self.assertEqual(expected_output, output.decode("utf-8"))
 
     def test_kumparanian_ds_verify(self):
         with open("tests/kumparanian_ds_verify.output") as file:
             expected_output = file.read()
-        command = "python -m kumparanian ds verify; exit 0"
+        command = "kumparanian ds verify; exit 0"
         output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
         self.assertEqual(expected_output, output.decode("utf-8"))
 
     def test_kumparanian_ds_verify_help(self):
         with open("tests/kumparanian_ds_verify_help.output") as file:
             expected_output = file.read()
-        command = "python -m kumparanian ds verify --help"
+        command = "kumparanian ds verify --help"
         output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
         self.assertEqual(expected_output, output.decode("utf-8"))
 
@@ -49,7 +49,7 @@ class TestCLI(unittest.TestCase):
         with open("tests/kumparanian_ds_verify_valid_model.output") as file:
             expected_output = file.read()
         command = (
-            "python -m kumparanian ds verify tests/sklearn_valid_model.pickle "
+            "kumparanian ds verify tests/sklearn_valid_model.pickle "
             "tests/vectorizer_label_encoder.pickle"
         )
         output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
@@ -58,7 +58,7 @@ class TestCLI(unittest.TestCase):
     def test_kumparanian_ds_verify_valid_tf_model(self):
         with open("tests/kumparanian_ds_verify_valid_model.output") as file:
             expected_output = file.read()
-        command = "python -m kumparanian ds verify tests/tf_valid_model.keras tests/tokenizer_label_encoder.pickle"
+        command = "kumparanian ds verify tests/tf_valid_model.keras tests/tokenizer_label_encoder.pickle"
         output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
         self.assertEqual(expected_output, output.decode("utf-8"))
 
@@ -66,7 +66,7 @@ class TestCLI(unittest.TestCase):
         with open("tests/kumparanian_ds_verify_invalid_pickle_file.output") as file:
             expected_output = file.read()
         command = (
-            "python -m kumparanian ds verify tests/invalid_pickle.pickle "
+            "kumparanian ds verify tests/invalid_pickle.pickle "
             "tests/tokenizer_label_encoder.pickle; exit 0"
         )
         output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
@@ -76,7 +76,7 @@ class TestCLI(unittest.TestCase):
         with open("tests/kumparanian_ds_verify_invalid_tokenizer.output") as file:
             expected_output = file.read()
         command = (
-            "python -m kumparanian ds verify tests/tf_valid_model.keras "
+            "kumparanian ds verify tests/tf_valid_model.keras "
             "tests/invalid_tokenizer.pickle; exit 0"
         )
         output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
@@ -86,7 +86,7 @@ class TestCLI(unittest.TestCase):
         with open("tests/kumparanian_ds_verify_invalid_vectorizer.output") as file:
             expected_output = file.read()
         command = (
-            "python -m kumparanian ds verify tests/sklearn_valid_model.pickle "
+            "kumparanian ds verify tests/sklearn_valid_model.pickle "
             "tests/invalid_vectorizer.pickle; exit 0"
         )
         output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
@@ -96,7 +96,7 @@ class TestCLI(unittest.TestCase):
         with open("tests/kumparanian_ds_verify_invalid_label_encoder.output") as file:
             expected_output = file.read()
         command = (
-            "python -m kumparanian ds verify tests/tf_valid_model.keras "
+            "kumparanian ds verify tests/tf_valid_model.keras "
             "tests/invalid_label_encoder.pickle; exit 0"
         )
         output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
@@ -106,7 +106,7 @@ class TestCLI(unittest.TestCase):
         with open("tests/kumparanian_ds_verify_invalid_model_predict.output") as file:
             expected_output = file.read()
         command = (
-            "python -m kumparanian ds verify tests/invalid_model.pickle "
+            "kumparanian ds verify tests/invalid_model.pickle "
             "tests/vectorizer_label_encoder.pickle; exit 0"
         )
         output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
@@ -115,14 +115,14 @@ class TestCLI(unittest.TestCase):
     def test_kumparanian_ds_evaluate(self):
         with open("tests/kumparanian_ds_evaluate.output") as file:
             expected_output = file.read()
-        command = "python -m kumparanian ds evaluate; exit 0"
+        command = "kumparanian ds evaluate; exit 0"
         output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
         self.assertEqual(expected_output, output.decode("utf-8"))
 
     def test_kumparanian_ds_evaluate_help(self):
         with open("tests/kumparanian_ds_evaluate_help.output") as file:
             expected_output = file.read()
-        command = "python -m kumparanian ds evaluate --help"
+        command = "kumparanian ds evaluate --help"
         output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
         self.assertEqual(expected_output, output.decode("utf-8"))
 
@@ -130,7 +130,7 @@ class TestCLI(unittest.TestCase):
         with open("tests/kumparanian_ds_evaluate_model.output") as file:
             expected_output = file.read()
         command = (
-            "python -m kumparanian ds evaluate tests/sklearn_valid_model.pickle "
+            "kumparanian ds evaluate tests/sklearn_valid_model.pickle "
             "tests/vectorizer_label_encoder.pickle; exit 0"
         )
         output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
@@ -140,7 +140,7 @@ class TestCLI(unittest.TestCase):
         with open("tests/kumparanian_ds_evaluate_model_test.output") as file:
             expected_output = file.read()
         command = (
-            "python -m kumparanian ds evaluate tests/tf_valid_model.keras tests/tokenizer_label_encoder.pickle "
+            "kumparanian ds evaluate tests/tf_valid_model.keras tests/tokenizer_label_encoder.pickle "
             "tests/test_set.csv; exit 0"
         )
         output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
